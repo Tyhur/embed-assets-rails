a2aec1c33bafb94878d032ee148f519548a40c2e
Eugeniy <altuh-78@mail.ru>
2016-04-29 12:28:34 +0500

    Update railtie.rb

--- lib/embed-assets-rails/railtie.rb
+++ lib/embed-assets-rails/railtie.rb
@@ -1,9 +1,9 @@
 module Saulabs::EmbedAssets
   class Railtie < ::Rails::Railtie
 
-    initializer "saulabs.sprockets.embed_assets", :after => 'sprockets.environment' do |app|
-      app.config.assets.embed_assets = false if app.config.assets.embed_assets.nil?
-      app.assets.register_bundle_processor 'text/css', Saulabs::EmbedAssets::Processor
+    initializer "saulabs.sprockets.embed_assets", :after => 'sprockets.environment' do 
+      Rails.application.config.assets.embed_assets = false if app.config.assets.embed_assets.nil?
+      Rails.application.assets.register_bundle_processor 'text/css', Saulabs::EmbedAssets::Processor
     end
 
   end
